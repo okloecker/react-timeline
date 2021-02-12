5c2bf4e268f78b7c7698c4d55a3870426fac9f22
Olaf Klöcker <npmokl@gmx.com>
2021-02-12 11:30:22 +0000

    feat: implement timeline with storybook stories

--- src/index.js
+++ src/index.js
@@ -1,6 +1,137 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState } from "react";
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const formatSecsToFriendly = (s) =>
+  `${Math.floor(s / 60)
+    .toFixed(0)
+    .toString()}:${Math.floor(s % 60)
+    .toFixed(0)
+    .toString()
+    .padStart(2, "0")}`;
+
+const Timeline = ({
+  currentTime = 0,
+  totalDuration = 0,
+  onSetNewTime,
+  height = 50,
+  width = 300,
+  padding = 10,
+  color = "black",
+}) => {
+  const [mouseEntered, setMouseEntered] = useState(false);
+  const [mouseTime, setMouseTime] = useState(0);
+  const [mousePercent, setMousePercent] = useState(0);
+  return (
+    <div
+      id="timelineContainer"
+      style={{
+        position: "relative",
+        paddingLeft: `${padding}px`,
+        paddingRight: `${padding}px`,
+        width,
+        height,
+        cursor: "pointer",
+      }}
+      onMouseEnter={(_) => setMouseEntered(true)}
+      onMouseLeave={(_) => setMouseEntered(false)}
+      onMouseMoveCapture={(e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const mousePercent = Math.max(
+          0,
+          Math.min(
+            1,
+            (e.clientX - rect.left - padding) / (rect.width - 2 * padding)
+          )
+        );
+        const mouseTime = Math.max(
+          0,
+          Math.min(totalDuration, totalDuration * mousePercent)
+        );
+        setMouseTime(mouseTime);
+        setMousePercent(mousePercent);
+      }}
+      onClick={(e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const currentTimePercent =
+          (e.clientX - rect.left - padding) / (rect.width - 2 * padding);
+        onSetNewTime(totalDuration * currentTimePercent);
+      }}
+    >
+      <div
+        id="timelineTimes"
+        style={{
+          color,
+          position: "absolute",
+          top: height <= 60 ? "5%" : "15%",
+          fontSize: "smaller",
+          userSelect: "none",
+        }}
+      >
+        {`${formatSecsToFriendly(currentTime)} / ${formatSecsToFriendly(
+          totalDuration
+        )}`}
+      </div>
+      <div
+        id="timelineBar"
+        style={{
+          position: "absolute",
+          bottom: "20%",
+          width: "inherit",
+          height: 6,
+          background: "#6f6f6f",
+        }}
+      >
+        {!!mouseEntered && (
+          <div
+            id="timelineDot"
+            style={{
+              position: "absolute",
+              top: -3,
+              left: `calc(${(100 * currentTime) / totalDuration}% - 6px)`,
+              width: 12,
+              height: 12,
+              background: color,
+              borderRadius: "50%",
+              color,
+            }}
+          />
+        )}
+        {totalDuration !== undefined && totalDuration !== null && (
+          <>
+            <div
+              id="timelineIndicator"
+              style={{
+                position: "absolute",
+                bottom: 0,
+                width: `calc(${(100 * currentTime) / totalDuration}%)`,
+                height: 6,
+                background: color,
+                color,
+                borderRadius: 4,
+              }}
+            />
+            {!!mouseEntered && (
+              <div
+                id="timelineTooltip"
+                style={{
+                  position: "absolute",
+                  top: -16,
+                  left:
+                    height >= 50
+                      ? `calc(${100 * mousePercent}% * 0.94)`
+                      : "50%",
+                  color,
+                  fontSize: "small",
+                  userSelect: "none",
+                }}
+              >
+                {formatSecsToFriendly(mouseTime)}
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;
---
+++ src/stories/index.stories.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { action } from "@storybook/addon-actions";
+import Timeline from "..";
+
+export default {
+  title: "Timeline",
+  component: Timeline,
+};
+
+export const TimelineDefault = () => (
+  <Timeline onSetNewTime={action("onSetNewTime")} />
+);
+
+export const Timeline100 = () => (
+  <Timeline
+    currentTime={100}
+    totalDuration={1959}
+    onSetNewTime={action("onSetNewTime")}
+  />
+);
+
+export const TimelineInteractive = () => {
+  const [time, setTime] = useState(0);
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "linear-gradient(to top,rgba(0,0,0,.8),rgba(0,0,0,0.3))",
+      }}
+    >
+      <Timeline
+        currentTime={time}
+        totalDuration={1800}
+        onSetNewTime={setTime}
+        height={80}
+        width={500}
+        padding={50}
+        color="white"
+      />
+    </div>
+  );
+};
+
+export const TimelineInteractiveWhite = () => {
+  const [time, setTime] = useState(0);
+  return (
+    <Timeline
+      currentTime={time}
+      totalDuration={1800}
+      onSetNewTime={setTime}
+      width={500}
+      height={100}
+      padding={50}
+    />
+  );
+};
+
+export const TimelineInteractiveNarrow = () => {
+  const [time, setTime] = useState(0);
+  return (
+    <div
+      style={{
+        display: "inline-block",
+        backgroundImage:
+          "linear-gradient(to top,rgba(0,0,0,.8),rgba(0,0,0,0.3))",
+      }}
+    >
+      <Timeline
+        currentTime={time}
+        totalDuration={1800}
+        onSetNewTime={setTime}
+        width={100}
+        height={50}
+        padding={3}
+        color="white"
+      />
+    </div>
+  );
+};
